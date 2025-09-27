90004009c5d3bea908b8103dcb84bd4fbb500def
jaeha-mac <jaeha-mac@jaeha-macui-MacBookPro.local>
2025-09-28 00:24:44 +0900

    fmt: terraform formatting

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,10 @@ terraform {
 
   # 원격 상태(backend) 설정: S3 + DynamoDB Lock
   backend "s3" {
-    bucket         = "my-terrastate"                         # ← 강의에서 만든 S3 버킷 이름
-    key            = "github-actions/terraform.tfstate"     # ← 버킷 내부 경로/파일명
-    region         = "ap-northeast-2"                            # ← 버킷 리전
-    dynamodb_table = "tf-resource-gha-lock"                            # ← 강의에서 만든 DynamoDB 테이블(Partition key: LockID)
+    bucket         = "my-terrastate"                    # ← 강의에서 만든 S3 버킷 이름
+    key            = "github-actions/terraform.tfstate" # ← 버킷 내부 경로/파일명
+    region         = "ap-northeast-2"                   # ← 버킷 리전
+    dynamodb_table = "tf-resource-gha-lock"             # ← 강의에서 만든 DynamoDB 테이블(Partition key: LockID)
     encrypt        = true
   }
 }
